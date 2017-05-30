d286e9d8b35f2b83e97a22716ad7bd72b3553dce
Daniel <danvelcab@gmail.com>
2017-05-30 20:08:07 +0200

    Model namespace changed to "App"

--- facebook-login-project-ws/app/Gift.php
+++ facebook-login-project-ws/app/Gift.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Gift;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
--- facebook-login-project-ws/app/Type.php
+++ facebook-login-project-ws/app/Type.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Type;
+namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
